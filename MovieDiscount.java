9d999e3f641b073f91e3ec0e3d061e2114f45081
Carter VanBommel <71711972+CarterVanBommel@users.noreply.github.com>
2020-10-05 09:30:20 -0400

    Redid the code

--- MovieDiscount.java
+++ MovieDiscount.java
@@ -1,25 +1,52 @@
 /*
- * Programmer:
- * Date:
- * Purpose:
+ /*
+ * Programmer: Carter Van Bommel
+ * Date: October 5, 2020
+ * Purpose: Calculates the cost of a ticket
  */
-
+ 
+// Imports Scanner
 import java.util.Scanner;
 
 public class MovieDiscount {
     public static void main (String [] args) {
+    	
         // Create the input scanner
         Scanner input = new Scanner(System.in);
 
-        // Ask the user for the side lengths
+        
+        // Defines ticketCost as 14.50
+        double ticketCost = 14.50;
+        
+        
+        // Ask the user for the their age
         System.err.print("How old are you? ");
         int age = input.nextInt();
 
+        
         // Ask the user if they have a coupon
         System.err.print("Do you have a coupon? [true/false] ");
-        // TODO: Read in a true/false value
+        boolean hasCoupon = input.nextBoolean();
 
+        
+        // If the user is in this age range, lower price by $3
+        if (age >= 13 || age <= 65) {
+        	ticketCost -= 3;	
+        	System.err.println(ticketCost);
+        }
+        
+        // If the user has a ticket, lower price by $2
+        if (hasCoupon = true) {
+        	ticketCost -= 2;
+        	System.err.println(ticketCost);
+        }
+        
+        
         // Print the cost of a movie ticket
-        // TODO: Figure out the cost and print it
+        System.out.println("Your ticket costs $" + ticketCost + "."); 
+        
+        
+        input.close();
+        
     }
 }
